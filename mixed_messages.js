d7e618e940e8ac59659b7b3eea9b0b92992b303e
Lewis <lslewissimmonds@googlemail.com>
2022-02-01 00:50:43 +0000

    define arrays

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,2 +1,8 @@
-console.log('Hello World!');
+const starSigns = ['cancer','gemini','taurus','pisces','leo','libra',
+'sagittarius','aquarius','scorpio','capricorn','virgo','aries'];
 
+const moods = ['happy','sad','angry','fearful','joyful','excited','anxious',
+'worried','hopeful','loved','confused','grateful'];
+
+const outcome = ['have fun','smile','laugh','love','cry','explore','go for it',
+'do what feels right','be cautious','go on an adventure','give up','carry on'];
