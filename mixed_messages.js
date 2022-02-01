b396a47523e80fe908940ce02fc6d36e1e9cb2b3
Lewis <lslewissimmonds@googlemail.com>
2022-02-01 01:50:08 +0000

    final code

--- mixed_messages.js
+++ mixed_messages.js
@@ -7,7 +7,15 @@ const moods = ['happy','sad','angry','fearful','joyful','excited','anxious',
 const outcome = ['have fun','smile','laugh','love','cry','explore','go for it',
 'do what feels right','be cautious','go on an adventure','give up','carry on'];
 
-let randomElement = (array) => {
+const randomElement = (array) => {
     return array[Math.floor(Math.random() * 12)];
+};
+
+const finalOutput = () => {
+    let starSign = randomElement(starSigns);
+    let mood = randomElement(moods);
+    let action = randomElement(outcome);
+    return `Your star sign is ${starSign}. You are most likely to be ${mood}. 
+    I think that you should ${action}.`;
 };
 
